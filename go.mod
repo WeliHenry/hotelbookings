1881442d3fc361eab406f7b6c04aea0643ff02fe
henry weli <henrysnoops@gmail.com>
2021-06-25 15:43:22 +0100

    rolled back the repo

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/welihenry/hotelbookings
 
 go 1.15
 
-require (
-	github.com/alexedwards/scs/v2 v2.4.0 // indirect
-	github.com/go-chi/chi/v5 v5.0.3 // indirect
-)
+require github.com/go-chi/chi/v5 v5.0.3
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -12,7 +12,7 @@ import (
 var functions = template.FuncMap{}
 
 func RenderTemplate(w http.ResponseWriter, tmpl string)  {
-	tc, err:= createTemplateCache()
+	tc, err:= CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,7 +35,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string)  {
 
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 	pages, err:= filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
